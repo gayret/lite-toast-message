a1021f79966c72ef4f5542561cc21b11cb70ec5e
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-04-11 17:38:42 +0300

    refactor: duration added, es module was conveniently exported

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 /**
- * This module exports a function that creates a toast message object with 
+ * This module exports a function that creates a toast message object with
  * methods for displaying success, error, warning, and info messages.
  * @module message
  */
 
-import './index.css';
+import "./index.css";
 
 /**
  * Creates a toast message object.
  * @returns {Object} - An object containing methods for displaying different types of toast messages.
  */
 function message() {
-  const container = document.createElement('div');
-  container.classList.add('toast-container');
+  const container = document.createElement("div");
+  container.classList.add("toast-container");
   document.body.appendChild(container);
 
   /**
@@ -20,13 +20,14 @@ function message() {
    * @param {string} type - The type of toast to display (success, error, warning, or info).
    * @param {string} message - The message to display in the toast.
    * @param {string} [title] - An optional title to display in the toast header.
+   * @param {number} [duration] - The duration in milliseconds to display the toast.
    */
-  function show(type, message, title) {
-    const toast = document.createElement('div');
-    toast.classList.add('toast', `toast-${type}`, 'hide');
+  function show(type, message, title, duration) {
+    const toast = document.createElement("div");
+    toast.classList.add("toast", `toast-${type}`, "hide");
     toast.innerHTML = `
       <div class="toast-header">
-        <span class="toast-title">${title !== undefined ? title : ''}</span>
+        <span class="toast-title">${title !== undefined ? title : ""}</span>
       </div>
       <div class="toast-body">
         ${message}
@@ -35,13 +36,16 @@ function message() {
     container.appendChild(toast);
     toast.style.top = `calc(50% - ${toast.offsetHeight / 2}px)`;
     toast.style.left = `calc(50% - ${toast.offsetWidth / 2}px)`;
-    setTimeout(() => {
-      toast.classList.remove('hide');
-      toast.classList.add('show');
-    }, 100);
-    setTimeout(() => {
-      container.removeChild(toast);
-    }, 3000);
+    toast.classList.remove("hide");
+    setTimeout(
+      () => {
+        toast.classList.add("hide");
+        setTimeout(() => {
+          toast.remove();
+        }, 500);
+      },
+      duration !== undefined ? duration : 3000
+    );
   }
 
   return {
@@ -49,37 +53,41 @@ function message() {
      * Displays a success toast message with the given message and title.
      * @param {string} message - The message to display in the toast.
      * @param {string} [title] - An optional title to display in the toast header.
+     * @param {number} [duration] - The duration in milliseconds to display the toast.
      */
-    success: function (message, title) {
-      show('success', message, title);
+    success(message, title, duration) {
+      show("success", message, title, duration);
     },
     /**
      * Displays an error toast message with the given message and title.
      * @param {string} message - The message to display in the toast.
      * @param {string} [title] - An optional title to display in the toast header.
+     * @param {number} [duration] - The duration in milliseconds to display the toast.
      */
-    error: function (message, title) {
-      show('error', message, title);
+    error(message, title, duration) {
+      show("error", message, title, duration);
     },
     /**
      * Displays a warning toast message with the given message and title.
      * @param {string} message - The message to display in the toast.
      * @param {string} [title] - An optional title to display in the toast header.
+     * @param {number} [duration] - The duration in milliseconds to display the toast.
      */
-    warning: function (message, title) {
-      show('warning', message, title);
+    warning(message, title, duration) {
+      show("warning", message, title, duration);
     },
     /**
      * Displays an info toast message with the given message and title.
      * @param {string} message - The message to display in the toast.
      * @param {string} [title] - An optional title to display in the toast header.
+     * @param {number} [duration] - The duration in milliseconds to display the toast.
      */
-    info: function (message, title) {
-      show('info', message, title);
+    info(message, title, duration) {
+      show("info", message, title, duration);
     },
   };
 }
 
-const toastMessage = message();
+const { success, error, warning, info } = message();
 
-module.exports = toastMessage;
+export { success, error, warning, info };
